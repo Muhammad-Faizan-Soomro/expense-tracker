db287bf51b326b1091cca930599fbf5a041d1021
Muhammad Faizan Soomro <mfaizansoomro00@gmail.com>
2025-03-19 03:00:40 +0500

    add 'add expense' functionality

--- expense-tracker.js
+++ expense-tracker.js
@@ -17,3 +17,37 @@ const readFile = () => {
   const data = fs.readFileSync(EXPENSE_FILE, "utf-8");
   return data ? JSON.parse(data) : [];
 };
+
+const addExpense = (description, amount) => {
+  try {
+    description = description.trim();
+    if (!description) {
+      throw new Error("description cannot be empty.");
+    }
+
+    amount = Number(amount);
+    if (isNaN(amount) || amount < 0) {
+      throw new Error(
+        "Invalid amount, please provide a valid positive numeric amount."
+      );
+    }
+
+    const now = new Date().toLocaleDateString();
+
+    const data = readFile();
+
+    const expense = {
+      id: data.length === 0 ? 1 : Math.max(...data.map((data) => data.id)) + 1,
+      description: description,
+      amount: amount,
+      createdAt: now,
+      updatedAt: now,
+    };
+
+    writeFile([...data, expense]);
+
+    console.log(`Expense added successfully (ID: ${expense.id})`);
+  } catch (error) {
+    console.error(`Error adding expense: ${error.message}`);
+  }
+};
